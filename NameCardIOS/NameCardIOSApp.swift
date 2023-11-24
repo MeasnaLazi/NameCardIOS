a2ea77e83978de9a72dd0cfff1b090b1cb0c38c0
sna_lodosha <measna.ly@lodosha.jp>
2023-11-24 10:10:47 -0600

    initialize project

---
+++ NameCardIOS/NameCardIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameCardIOSApp.swift
+//  NameCardIOS
+//
+//  Created by Measna on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NameCardIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NameCardIOSUITests/NameCardIOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NameCardIOSUITestsLaunchTests.swift
+//  NameCardIOSUITests
+//
+//  Created by Measna on 24/11/23.
+//
+
+import XCTest
+
+final class NameCardIOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
