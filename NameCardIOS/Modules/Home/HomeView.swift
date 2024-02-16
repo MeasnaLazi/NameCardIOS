47dc1d4018c2eea6b66ec296efc8ac4f20baff94
sna_lodosha <measna.ly@lodosha.jp>
2024-02-15 21:30:39 -0600

    add separator to tab view and change font

--- NameCardIOS/Modules/Home/HomeView.swift
+++ NameCardIOS/Modules/Home/HomeView.swift
@@ -9,35 +9,52 @@ import SwiftUI
 
 struct HomeView : View {
     
+    @State private var selectedTab = "0"
+    
     init() {
         UITabBar.appearance().backgroundColor = .white
         UITabBar.appearance().unselectedItemTintColor = .unselect
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.init(name: "Inter-Regular", size: 10)! ], for: .normal)
     }
     
     var body: some View {
-        TabView {
-            NameCardView()
-                .tabItem {
-                    Label("Card", systemImage: "creditcard.circle")
-                }
-            EventView()
-                .tabItem {
-                    Label("Event", systemImage: "circle.hexagongrid.circle")
-                }
-            DealView()
-                .tabItem {
-                    Label("Deal", systemImage: "dollarsign.circle")
-                }
-            NotificationView()
-                .tabItem {
-                    Label("Notifications", systemImage: "bell.circle")
-                }
-            MenuView()
-                .tabItem {
-                    Label("Menu", systemImage: "list.bullet.circle")
-                }
+        ZStack {
+            TabView(selection: $selectedTab) {
+                NameCardView()
+                    .tabItem {
+                        Label("Card", systemImage: "creditcard.circle")
+                    }
+                    .tag("0")
+                EventView()
+                    .tabItem {
+                        Label("Event", systemImage: "circle.hexagongrid.circle")
+                    }
+                    .tag("1")
+                DealView()
+                    .tabItem {
+                        Label("Deal", systemImage: "dollarsign.circle")
+                    }
+                    .tag("2")
+                NotificationView()
+                    .tabItem {
+                        Label("Notifications", systemImage: "bell.circle")
+                    }
+                    .tag("3")
+                MenuView()
+                    .tabItem {
+                        Label("Menu", systemImage: "list.bullet.circle")
+                    }
+                    .tag("4")
+            }
+            .accentColor(.app)
+            
+            VStack {
+                Spacer()
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 1)
+                    .foregroundColor(.border)
+                    .padding(.bottom, 60)
+            }
         }
-        .accentColor(.app)
     }
-    
 }
